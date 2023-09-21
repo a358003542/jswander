855bbd966aa002c3306559b66b7796cbbdc90cd4
wanze <a358003542@outlook.com>
2023-09-21 16:47:07 +0800

    init

---
+++ module.js
@@ -0,0 +1,9 @@
+function sayhello(){
+    console.log('hello');
+}
+
+function add1(num){
+    return num+1;
+}
+
+export {sayhello, add1};
---
+++ module.test.js
@@ -0,0 +1,6 @@
+import {add1} from './module.js';
+
+
+test('return number add one', ()=>{
+    expect(add1(1)).toBe(2);
+})
